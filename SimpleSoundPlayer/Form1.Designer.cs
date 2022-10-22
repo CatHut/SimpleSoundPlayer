3381d7c66e59b827a2a2dcdbdefba892ef07fa08
CatHut <necota2496@gmail.com>
2022-10-22 22:22:36 +0900

    サウンドプレイヤーを作成

---
+++ SimpleSoundPlayer/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace SimpleSoundPlayer
+{
+    partial class Form_SimpleSoundPlayer
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_FolderPath = new System.Windows.Forms.TextBox();
+            this.listView_FileList = new System.Windows.Forms.ListView();
+            this.columnHeader_FileName = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader_No = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader_Length = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader_Ext = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader_FolderPath = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader_FullPath = new System.Windows.Forms.ColumnHeader();
+            this.SuspendLayout();
+            // 
+            // textBox_FolderPath
+            // 
+            this.textBox_FolderPath.AllowDrop = true;
+            this.textBox_FolderPath.Location = new System.Drawing.Point(12, 12);
+            this.textBox_FolderPath.Name = "textBox_FolderPath";
+            this.textBox_FolderPath.Size = new System.Drawing.Size(920, 23);
+            this.textBox_FolderPath.TabIndex = 0;
+            this.textBox_FolderPath.TextChanged += new System.EventHandler(this.textBox_FolderPath_TextChanged);
+            this.textBox_FolderPath.DragDrop += new System.Windows.Forms.DragEventHandler(this.textBox_FolderPath_DragDrop);
+            this.textBox_FolderPath.DragEnter += new System.Windows.Forms.DragEventHandler(this.textBox_FolderPath_DragEnter);
+            // 
+            // listView_FileList
+            // 
+            this.listView_FileList.AllowDrop = true;
+            this.listView_FileList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader_FileName,
+            this.columnHeader_No,
+            this.columnHeader_Length,
+            this.columnHeader_Ext,
+            this.columnHeader_FolderPath,
+            this.columnHeader_FullPath});
+            this.listView_FileList.Location = new System.Drawing.Point(12, 41);
+            this.listView_FileList.MultiSelect = false;
+            this.listView_FileList.Name = "listView_FileList";
+            this.listView_FileList.Size = new System.Drawing.Size(920, 548);
+            this.listView_FileList.TabIndex = 2;
+            this.listView_FileList.UseCompatibleStateImageBehavior = false;
+            this.listView_FileList.View = System.Windows.Forms.View.Details;
+            this.listView_FileList.SelectedIndexChanged += new System.EventHandler(this.listView_FileList_SelectedIndexChanged);
+            this.listView_FileList.DragDrop += new System.Windows.Forms.DragEventHandler(this.textBox_FolderPath_DragDrop);
+            this.listView_FileList.DragEnter += new System.Windows.Forms.DragEventHandler(this.textBox_FolderPath_DragEnter);
+            this.listView_FileList.KeyDown += new System.Windows.Forms.KeyEventHandler(this.listView_FileList_KeyDown);
+            this.listView_FileList.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listView_FileList_MouseClick);
+            // 
+            // columnHeader_FileName
+            // 
+            this.columnHeader_FileName.DisplayIndex = 1;
+            this.columnHeader_FileName.Text = "ファイル名";
+            this.columnHeader_FileName.Width = 180;
+            // 
+            // columnHeader_No
+            // 
+            this.columnHeader_No.DisplayIndex = 0;
+            this.columnHeader_No.Text = "No.";
+            this.columnHeader_No.Width = 45;
+            // 
+            // columnHeader_Length
+            // 
+            this.columnHeader_Length.Text = "長さ";
+            // 
+            // columnHeader_Ext
+            // 
+            this.columnHeader_Ext.Text = "形式";
+            // 
+            // columnHeader_FolderPath
+            // 
+            this.columnHeader_FolderPath.Text = "フォルダパス";
+            this.columnHeader_FolderPath.Width = 600;
+            // 
+            // columnHeader_FullPath
+            // 
+            this.columnHeader_FullPath.Text = "フルパス";
+            this.columnHeader_FullPath.Width = 0;
+            // 
+            // Form_SimpleSoundPlayer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(944, 601);
+            this.Controls.Add(this.listView_FileList);
+            this.Controls.Add(this.textBox_FolderPath);
+            this.Name = "Form_SimpleSoundPlayer";
+            this.Text = "SimpleSoundPlayer";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form_SimpleSoundPlayer_FormClosing);
+            this.ResizeEnd += new System.EventHandler(this.Form_SimpleSoundPlayer_ResizeEnd);
+            this.Resize += new System.EventHandler(this.Form_SimpleSoundPlayer_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBox_FolderPath;
+        private ListView listView_FileList;
+        private ColumnHeader columnHeader_FileName;
+        private ColumnHeader columnHeader_Length;
+        private ColumnHeader columnHeader_FolderPath;
+        private ColumnHeader columnHeader_No;
+        private ColumnHeader columnHeader_Ext;
+        private ColumnHeader columnHeader_FullPath;
+    }
+}
---
+++ SimpleSoundPlayer/Form1.cs
@@ -0,0 +1,204 @@
+using System.Diagnostics;
+using WMPLib;
+using CatHut;
+
+namespace SimpleSoundPlayer
+{
+    public partial class Form_SimpleSoundPlayer : Form
+    {
+        WMPLib.WindowsMediaPlayer m_WMP = null;
+        AppSetting m_AppSetting;
+
+        enum COLUMNS
+        {
+            FILE_NAME =0,
+            NO,
+            Length,
+            EXT,
+            FOLDER_PATH,
+            FULL_PATH
+        }
+
+        public Form_SimpleSoundPlayer()
+        {
+            InitializeComponent();
+            m_AppSetting = new AppSetting();
+            textBox_FolderPath.Text = m_AppSetting.Settings.Path;
+            this.ActiveControl = this.listView_FileList;
+        }
+
+        private void textBox_FolderPath_DragDrop(object sender, DragEventArgs e)
+        {
+            if (!e.Data.GetDataPresent(DataFormats.FileDrop)) return;
+
+            string[] dragFilePathArr = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            textBox_FolderPath.Text = dragFilePathArr[0];
+
+        }
+
+        private void textBox_FolderPath_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.All;
+        }
+
+
+        private void listView_FileList_KeyDown(object sender, KeyEventArgs e)
+        {
+            //キーが押されたか調べる
+            if (e.KeyCode == Keys.Enter)
+            {
+                var selected = this.listView_FileList.SelectedItems;
+
+                if (selected.Count < 1)
+                {
+                    listView_FileList.Items[0].Selected = true;
+                    listView_FileList.Items[0].Focused = true;
+                    return;
+                }
+
+                var idx = listView_FileList.SelectedItems[0].Index;
+                var next = (idx + 1) % listView_FileList.Items.Count;
+
+                listView_FileList.Items[next].Selected = true;
+                listView_FileList.Items[next].Focused = true;
+                listView_FileList.EnsureVisible(next);
+
+            }
+        }
+
+        private void listView_FileList_MouseClick(object sender, MouseEventArgs e)
+        {
+            var selected = this.listView_FileList.SelectedItems;
+
+            if (selected.Count < 1)
+            {
+                return;
+            }
+
+            var file = selected[0].SubItems[(int)COLUMNS.FULL_PATH].Text;
+
+            if (m_WMP == null)
+            {
+                m_WMP = new WindowsMediaPlayer();
+            }
+            else
+            {
+                m_WMP.controls.stop();
+            }
+            m_WMP.URL = file;
+            m_WMP.controls.play();
+
+            if ((e.Button & MouseButtons.Right) == MouseButtons.Right)
+            {
+                OpenExplorer(file);
+            }
+        }
+
+        private void listView_FileList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var selected = this.listView_FileList.SelectedItems;
+
+            if(selected.Count < 1)
+            {
+                return;
+            }
+
+            var file = selected[0].SubItems[(int)COLUMNS.FULL_PATH].Text;
+
+            if (m_WMP == null)
+            {
+                m_WMP = new WindowsMediaPlayer();
+            }
+            else
+            {
+                m_WMP.controls.stop();
+            }
+            m_WMP.URL = file;
+            m_WMP.controls.play();
+
+        }
+
+        private void OpenExplorer(string path)
+        {
+            try
+            {
+                Process.Start("explorer.exe", @"/select,""" + path + @"""");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.GetType());
+            }
+        }
+
+        private void Form_SimpleSoundPlayer_ResizeEnd(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form_SimpleSoundPlayer_Resize(object sender, EventArgs e)
+        {
+            listView_FileList.Size = new Size(this.Width - 40, this.Height - 92);
+            textBox_FolderPath.Size = new Size(this.Width - 40, textBox_FolderPath.Height);
+        }
+
+        private void textBox_FolderPath_TextChanged(object sender, EventArgs e)
+        {
+            if (true == Directory.Exists(textBox_FolderPath.Text))
+            {
+                System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(textBox_FolderPath.Text);
+                IEnumerable<System.IO.FileInfo> files = di.EnumerateFiles("*", System.IO.SearchOption.AllDirectories);
+
+                listView_FileList.Items.Clear();
+
+                var idx = 1;
+
+                foreach (System.IO.FileInfo f in files)
+                {
+                    var ext = Path.GetExtension(f.Name);
+                    if (ext != @".mp3" && ext != @".wav")
+                    {
+                        continue;
+                    }
+
+                    var file = f.FullName;
+
+                    var item = new ListViewItem();
+                    item.Text = Path.GetFileNameWithoutExtension(f.FullName);
+
+                    item.SubItems.Add(idx.ToString());
+                    idx++;
+
+                    //再生時間
+                    var tfile = TagLib.File.Create(file);
+                    TimeSpan duration = tfile.Properties.Duration;
+                    item.SubItems.Add(duration.ToString(@"mm\:ss\.ff"));
+
+                    //形式
+                    item.SubItems.Add(Path.GetExtension(f.Name).Replace(".", ""));
+
+                    //フォルダ名
+                    item.SubItems.Add(Path.GetDirectoryName(file));
+
+                    //フルパス
+                    item.SubItems.Add(f.FullName);
+
+                    listView_FileList.Items.Add(item);
+                    listView_FileList.Focus();
+
+                    if (m_WMP != null)
+                    {
+                        m_WMP.controls.stop();
+                    }
+                }
+
+            }
+
+        }
+
+        private void Form_SimpleSoundPlayer_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            m_AppSetting.Settings.Path = textBox_FolderPath.Text;
+            m_AppSetting.Exit();
+        }
+    }
+}
